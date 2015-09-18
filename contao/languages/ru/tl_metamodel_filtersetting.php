60cea1d5d6c0f1880f5786712217d22d5a3f2c3d
Mini Model <minimodel@metamodel.me>
2015-09-18 10:32:21 +0200

    Updated translations from transifex.

---
+++ contao/languages/ru/tl_metamodel_filtersetting.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-11T08:02:14+02:00
+ */
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['no']                    = 'Чекбокс нет';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['radio']                 = 'Выбор радио-кнопкой';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['checkbox'] = 'Да/нет';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['yes']                   = 'Чекбокс да';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['ynfield']['0']          = 'Да/нет вместо имени атрибута';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['ynfield']['1']          = 'Показать да/нет вместо имени атрибута и предоставить имя атрибута в качестве заголовка.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['ynmode']['0']           = 'Модус';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['ynmode']['1']           = 'Сделать да/нет выбираемым';
+
