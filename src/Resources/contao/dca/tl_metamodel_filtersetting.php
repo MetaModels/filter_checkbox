141670866c2463cb5a93b3695430caf80eee46ba
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-12-27 14:25:46 +0100

    Update copyright

--- src/Resources/contao/dca/tl_metamodel_filtersetting.php
+++ src/Resources/contao/dca/tl_metamodel_filtersetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/filter_checkbox.
  *
- * (c) 2012-2016 The MetaModels team.
+ * (c) 2012-2017 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,12 +11,13 @@
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    MetaModels
- * @subpackage FilterCheckbox
+ * @subpackage FilterCheckboxBundle
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Andreas Isaak <info@andreas-isaak.de>
  * @author     David Molineus <mail@netzmacht.de>
  * @author     Christian de la Haye <service@delahaye.de>
- * @copyright  2012-2016 The MetaModels team.
+ * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
