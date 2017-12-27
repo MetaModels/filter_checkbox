9ec786a1671d550d50eefd5227e096c7e1706050
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-12-27 14:21:17 +0100

    Add tests

---
+++ tests/DependencyInjection/MetaModelsFilterCheckboxExtensionTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * * This file is part of MetaModels/filter_checkbox.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage FilterCheckboxBundle
+ * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\FilterCheckboxBundle\Test\DependencyInjection;
+
+use MetaModels\FilterCheckboxBundle\DependencyInjection\MetaModelsFilterCheckboxExtension;
+use MetaModels\FilterCheckboxBundle\FilterSetting\CheckboxFilterSettingTypeFactory;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * This test case test the extension.
+ */
+class MetaModelsFilterCheckboxExtensionTest extends TestCase
+{
+    /**
+     * Test that extension can be instantiated.
+     *
+     * @return void
+     */
+    public function testInstantiation()
+    {
+        $extension = new MetaModelsFilterCheckboxExtension();
+
+        $this->assertInstanceOf(MetaModelsFilterCheckboxExtension::class, $extension);
+        $this->assertInstanceOf(ExtensionInterface::class, $extension);
+    }
+
+    /**
+     * Test that the services are loaded.
+     *
+     * @return void
+     */
+    public function testFactoryIsRegistered()
+    {
+        $container = $this->getMockBuilder(ContainerBuilder::class)->getMock();
+
+        $container
+            ->expects($this->atLeastOnce())
+            ->method('setDefinition')
+            ->withConsecutive(
+                [
+                    'metamodels.filter_checkbox.factory',
+                    $this->callback(
+                        function ($value) {
+                            /** @var Definition $value */
+                            $this->assertInstanceOf(Definition::class, $value);
+                            $this->assertEquals(CheckboxFilterSettingTypeFactory::class, $value->getClass());
+                            $this->assertCount(1, $value->getTag('metamodels.filter_factory'));
+
+                            return true;
+                        }
+                    )
+                ]
+            );
+
+        $extension = new MetaModelsFilterCheckboxExtension();
+        $extension->load([], $container);
+    }
+}
---
+++ tests/DeprecatedAutoloaderTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * * This file is part of MetaModels/filter_checkbox.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage FilterCheckboxBundle
+ * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\FilterCheckboxBundle\Test;
+
+use MetaModels\FilterCheckboxBundle\FilterSetting\Checkbox as CheckboxSetting;
+use MetaModels\FilterCheckboxBundle\FilterSetting\CheckboxFilterSettingTypeFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This class tests if the deprecated autoloader works.
+ *
+ * @package MetaModels\AttributeSelectBundle\Test
+ */
+class DeprecatedAutoloaderTest extends TestCase
+{
+    /**
+     * Selectes of old classes to the new one.
+     *
+     * @var array
+     */
+    private static $classes = [
+        // FilterSetting
+        'MetaModels\Filter\Setting\Checkbox'                         => CheckboxSetting::class,
+        'MetaModels\Filter\Setting\CheckboxFilterSettingTypeFactory' => CheckboxFilterSettingTypeFactory::class,
+    ];
+
+    /**
+     * Provide the alias class map.
+     *
+     * @return array
+     */
+    public function provideAliasClassMap()
+    {
+        $values = [];
+
+        foreach (static::$classes as $select => $class) {
+            $values[] = [
+                $select,
+                $class,
+            ];
+        }
+
+        return $values;
+    }
+
+    /**
+     * Test if the deprecated classes are aliased to the new one.
+     *
+     * @param string $oldClass Old class name.
+     * @param string $newClass New class name.
+     *
+     * @dataProvider provideAliasClassMap
+     */
+    public function testDeprecatedClassesAreAliased($oldClass, $newClass)
+    {
+        $this->assertTrue(class_exists($oldClass), sprintf('Class select "%s" is not found.', $oldClass));
+
+        $oldClassReflection = new \ReflectionClass($oldClass);
+        $newClassReflection = new \ReflectionClass($newClass);
+
+        $this->assertSame($newClassReflection->getFileName(), $oldClassReflection->getFileName());
+    }
+}
