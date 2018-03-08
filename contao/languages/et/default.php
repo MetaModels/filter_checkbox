59787ccd556c8b9dba23cde0bb3808e3dcd29857
Mini Model <minimodel@metamodel.me>
2018-03-08 09:48:44 +0100

    Updated translations from transifex.

---
+++ contao/languages/et/default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
+ *
+ * last-updated: 2018-03-08T09:48:39+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['MSC']['extended_no'] = '%s: ei';
+
