9a34fc365cd002f3e9d3d334a64f0597762379d2
Mini Model <minimodel@metamodel.me>
2019-01-03 20:26:54 +0100

    Update PHPDoc file headers

--- contao/config/event_listeners.php
+++ contao/config/event_listeners.php
@@ -3,18 +3,17 @@
 /**
  * This file is part of MetaModels/filter_checkbox.
  *
- * (c) 2012-2016 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage FilterCheckbox
+ * @package    MetaModels/filter_checkbox
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2012-2016 The MetaModels team.
- * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
