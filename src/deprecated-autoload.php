7f81b30dcc150d8221f8571382ccc5bcf18c3850
Christian Schiffler <c.schiffler@cyberspectrum.de>
2019-01-18 08:53:07 +0100

    Code style

--- src/deprecated-autoload.php
+++ src/deprecated-autoload.php
@@ -3,27 +3,25 @@
 /**
  * This file is part of MetaModels/filter_checkbox.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage FilterCheckboxBundle
+ * @package    MetaModels/filter_checkbox
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
-
-// This hack is to load the "old locations" of the classes.
-
 
 use MetaModels\FilterCheckboxBundle\FilterSetting\Checkbox as CheckboxSetting;
 use MetaModels\FilterCheckboxBundle\FilterSetting\CheckboxFilterSettingTypeFactory;
 
+// This hack is to load the "old locations" of the classes.
 spl_autoload_register(
     function ($class) {
         static $classes = [
