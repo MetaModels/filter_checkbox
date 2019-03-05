03dbd93db48c69801859520a73fe0da73290688d
Mini Model <minimodel@metamodel.me>
2019-03-05 20:44:52 +0100

    Update PHPDoc file headers

--- src/MetaModelsFilterCheckboxBundle.php
+++ src/MetaModelsFilterCheckboxBundle.php
@@ -1,20 +1,19 @@
 <?php
 
 /**
- * * This file is part of MetaModels/filter_checkbox.
+ * This file is part of MetaModels/filter_checkbox.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage FilterCheckboxBundle
+ * @package    MetaModels/filter_checkbox
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
