c6117dcdac2a17e5d8de1facea9b2f8292426071
Ingolf Steinhardt <info@e-spin.de>
2021-08-01 21:11:12 +0200

    Switch to github-actions

--- src/EventListener/CheckboxFilterSettingTypeRendererListener.php
+++ src/EventListener/CheckboxFilterSettingTypeRendererListener.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/filter_checkbox.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2021 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,7 +12,8 @@
  *
  * @package    MetaModels/filter_checkbox
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2021 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_checkbox/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -25,6 +26,8 @@ use MetaModels\CoreBundle\EventListener\DcGeneral\Table\FilterSetting\AbstractFi
 
 /**
  * Handles rendering of model from tl_metamodel_filtersetting.
+ *
+ * @SuppressWarnings(PHPMD.LongClassName)
  */
 class CheckboxFilterSettingTypeRendererListener extends AbstractFilterSettingTypeRenderer
 {
